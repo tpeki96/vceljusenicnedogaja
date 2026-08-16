11a80c19cf7d6db34132f56f2d1371edea2741ad
tpeki96 <percic.tadej@gmail.com>
2026-08-16 23:20:49 +0200

    Add app layout

---
+++ app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "V Celju se nič ne dogaja",
+  description: "Družbeni eksperiment in pregled dogodkov v Celju.",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="sl">
+      <body>{children}</body>
+    </html>
+  );
+}
