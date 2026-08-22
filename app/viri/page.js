2733bdce823c213254a3b0d22118ddcb87fb90f5
tpeki96 <percic.tadej@gmail.com>
2026-08-22 19:07:09 +0200

    Add Slovenian sources route

---
+++ app/viri/page.js
@@ -0,0 +1,10 @@
+import SourcesPage from "../SourcesPage";
+
+export const metadata = {
+  title: "Viri | V Celju se nič ne dogaja",
+  description: "Pregled javnih virov, iz katerih zbiramo dogodke v Celju.",
+};
+
+export default function SourcesRoute() {
+  return <SourcesPage lang="sl" />;
+}
